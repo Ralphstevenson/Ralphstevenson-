e7718bd1e349b8c774677d1da2fc22e28b829d71
Ralphstevenson <156324823+Ralphstevenson@users.noreply.github.com>
2026-03-27 08:00:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -62,19 +62,7 @@ window.handleLogout = () => {
     }
 };
 
-// Fonksyon pou chanje ant Login ak Signup nan paj Auth la
-window.toggleAuth = (type) => {
-    const loginSection = document.getElementById('login-section');
-    const signupSection = document.getElementById('signup-section');
 
-    if (type === 'signup') {
-        loginSection.classList.add('hidden');
-        signupSection.classList.remove('hidden');
-    } else {
-        signupSection.classList.add('hidden');
-        loginSection.classList.remove('hidden');
-    }
-};
 
 // ==========================================
 // III. SISTÈM NOTIFIKASYON
